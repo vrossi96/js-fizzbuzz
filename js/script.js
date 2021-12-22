85f23b2e326a24d6c2f67f4604b920584db3f845
vrossi96 <vrossi396@yahoo.it>
2021-12-22 14:37:34 +0100

    aggiunta consegna e passi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 console.log('OK JS');
+
+/*
+
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+Prima di partire a scrivere codice poniamoci qualche domanda
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+BONUS:
+Provare a stampare la lista in pagina invece che in console
+
+
+Passi
+
+1- creare programma con cicli da 1 a 100 con for
+  1a- SE il numero è multiplo di 3 allora Fizz
+  1b- SE il numero è multiplo di 5 allora Buzz
+  1c- SE il numero è multiplo di 3 e 5 allora FizzBuzz
+
+*/
